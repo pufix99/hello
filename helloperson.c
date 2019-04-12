c89b1e6bef3d1c7b12d0dfdb48d265fa03fa40e4
Paula-Diana Bacircea <dianabacircea19@gmail.com>
2019-04-12 02:42:44 -0700

    Modified the file

--- helloperson.c
+++ helloperson.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-int main(int argc, char *argv[])
+int main()
 {
-if(argc!=2){
-printf("USAGE: %s name\n", argv[0]);
-return -1;
-}
-printf("Hello, %s!\n", argv[1]);
+char nume[30];
+scanf("%s", nume);
+printf("Hello, %s!\n", nume);
 return 0;
 }
